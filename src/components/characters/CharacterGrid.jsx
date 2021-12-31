52b66722018bf931b1cbbc0118e8cc3d9533fbf8
Sebastian <roshfeltsemo@gmail.com>
2021-12-31 02:19:20 -0500

    add loading gif

--- src/components/characters/CharacterGrid.jsx
+++ src/components/characters/CharacterGrid.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import CharacterItem from './CharacterItem';
+import Spinner from '../ui/Spinner';
 
 const CharacterGrid = ({items, isLoading}) => {
-    return isLoading ? (<h1>Loading...</h1>) : (
+    return isLoading ? (<Spinner />) : (
         <section className='cards'>
             {items.map(item => (
                 <CharacterItem item={item} key={item.char_id} />
